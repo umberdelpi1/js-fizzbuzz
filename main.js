706f9f36afb693d7ce2cacb067bda1871be73666
Umberto Del Piano <umberdelpi1@gmail.com>
2020-03-26 11:29:01 +0100

    Primo step multipli di 3 "Fizz" fatto

---
+++ main.js
@@ -0,0 +1,19 @@
+// Stampare numeri da 1 a 100
+for(var i = 1; i <= 100; i++) {
+
+console.log(i);
+
+if(i%3==0){
+
+console.log("Fizz " + i);
+}
+
+}
+
+// I numeri multipli di 3 stampare "Fizz" al posto del numero
+
+
+// I multipli di 5 stampare "Buzz" al posto del numero
+
+
+//  Per numeri sia multipli di 3 che 5 stampare "FizzBuzz"
