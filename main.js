b926ad49ea713442e8c5be0935a900674a5d30a6
Umberto Del Piano <umberdelpi1@gmail.com>
2020-03-26 12:05:22 +0100

    Variante con else if

--- main.js
+++ main.js
@@ -5,15 +5,21 @@ for(var i = 1; i <= 100; i++) {
 
   if(i%3==0){
   console.log("Fizz " + i)
-  }
-  
-   if (i%5==0){
-    console.log("Buzz" + i)
-  }
+} else if (i%5==0){
+   console.log("Buzz" + i)
+ }
+ if(i%15==0){
+   console.log("FizzBuzz" + i);
+ }
 
-  if(i%15==0){
-    console.log("FizzBuzz" + i);
-  }
+
+  //  if (i%5==0){
+  //   console.log("Buzz" + i)
+  // }
+  //
+  // if(i%15==0){
+  //   console.log("FizzBuzz" + i);
+  // }
 
 }
 
