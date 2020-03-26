03c6ae1d6c74ff68d7605c5ca369bf1be8f705d8
Umberto Del Piano <umberdelpi1@gmail.com>
2020-03-26 12:14:29 +0100

    Indentazione e variante solo if e else if

--- main.js
+++ main.js
@@ -2,32 +2,32 @@
 for(var i = 1; i <= 100; i++) {
 
   console.log(i);
-
+// I numeri multipli di 3 stampare "Fizz" al posto del numero
+// I multipli di 5 stampare "Buzz" al posto del numero
+//  Per numeri sia multipli di 3 che 5 stampare "FizzBuzz"
   if(i%3==0){
-  console.log("Fizz " + i)
-} else if (i%5==0){
-   console.log("Buzz" + i)
- }
- if(i%15==0){
-   console.log("FizzBuzz" + i);
- }
-
-
-  //  if (i%5==0){
-  //   console.log("Buzz" + i)
-  // }
-  //
-  // if(i%15==0){
-  //   console.log("FizzBuzz" + i);
-  // }
-
+  console.log("Fizz: " + i)
+  } else if (i%5==0){
+   console.log("Buzz: " + i)
+  }
+  if(i%15==0){
+   console.log("FizzBuzz: " + i);
+  }
 }
 
-
-// I numeri multipli di 3 stampare "Fizz" al posto del numero
-
-
-// I multipli di 5 stampare "Buzz" al posto del numero
-
-
-//  Per numeri sia multipli di 3 che 5 stampare "FizzBuzz"
+// for(var i = 1; i <= 100; i++) {
+//
+//   console.log(i);
+// // I numeri multipli di 3 stampare "Fizz" al posto del numero
+//   if(i%3==0){
+//   console.log("Fizz: " + i)
+//   }
+// // I multipli di 5 stampare "Buzz" al posto del numero
+//   if (i%5==0){
+//    console.log("Buzz: " + i)
+//   }
+// //  Per numeri sia multipli di 3 che 5 stampare "FizzBuzz"
+//   if(i%15==0){
+//    console.log("FizzBuzz: " + i);
+//   }
+// }
